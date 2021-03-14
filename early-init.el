575a217ce629b487cc461e0ccfea943f87479f52
Christian Dijkstra <chdka@public-files.de>
2021-03-14 16:43:31 +0100

    chdka-emacs early-init.el : Start emacs maximised
    This works in non-server mode, when in server-mode the changes do not
    work on MS Windows

--- early-init.el
+++ early-init.el
@@ -71,10 +71,14 @@
 (message "early-init.el @ GUI..")
 
 ;; Initialize GUI elements
+(add-to-list 'initial-frame-alist '(fullscreen . maximised))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 (menu-bar-mode 1)
 (tool-bar-mode -1)
-(scroll-bar-mode 1)
+(scroll-bar-mode -1)
 (setq inhibit-splash-screen t)
+
 
 
 ;; END: 
